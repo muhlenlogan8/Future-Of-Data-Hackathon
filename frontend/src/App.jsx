4ad92475868683bac7e44284f80b12eeda41e0ba
muhlenlogan8 <muhlenlogan8@gmail.com>
2025-09-06 15:53:54 -0400

    fixing app issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,16 @@ import Admin from "./pages/Admin";
 import Login from "./pages/Login";
 
 const App = () => {
-	return <div></div>;
+	return (
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/projects" element={<Projects />} />
+			<Route path="/admin" element={<Admin />} />
+			<Route path="/form" element={<Form />} />
+			<Route path="/profile" element={<Profile />} />
+			<Route path="/signin" element={<Login />} />
+		</Routes>
+	);
 };
 
 export default App;
